e0c4893c86b575a3c235dce7f2482ffe3181abae
cjrumble <cjrumble@gmail.com>
2016-04-04 22:57:01 -0700

    Deitel
    Ch2_6

---
+++ src/Welcome4.java
@@ -0,0 +1,11 @@
+// Figure 2.6: Welcome4.java
+// Displaying multiple lines with method System.out.printf.
+
+public class Welcome4
+{
+	// main method begins execution of the Java application
+	public static void main(String[] args)
+	{
+		System.out.printf("%s%n%s%n","Welcome to","Java Programming!");
+	} // end main method
+} // end Welcome4 class
